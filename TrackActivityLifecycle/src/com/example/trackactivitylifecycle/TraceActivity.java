3d2b221e01fd30edfeb305934fca7d87724cca83
hccarpen <hccarpen@udel.edu>
2015-04-28 12:29:47 -0400

    fixed method names

--- TrackActivityLifecycle/src/com/example/trackactivitylifecycle/TraceActivity.java
+++ TrackActivityLifecycle/src/com/example/trackactivitylifecycle/TraceActivity.java
@@ -12,33 +12,39 @@ public class TraceActivity extends Activity {
 		Log.i("Tracer","On Create Executed");
 	}
 	
-	protected void onStart(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+	@Override
+	protected void onStart() {
+		super.onStart();
 		Log.i("Tracer","On Start Executed");
 	}
 	
-	protected void onResume(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+	@Override
+	protected void onResume() {
+		super.onResume();
 		Log.i("Tracer","On Resume Executed");
 	}
 	
-	protected void onRestart(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+	@Override
+	protected void onRestart() {
+		super.onRestart();
 		Log.i("Tracer","On Restart Executed");
 	}
 	
-	protected void onPause(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+	@Override
+	protected void onPause() {
+		super.onPause();
 		Log.i("Tracer","On Pause Executed");
 	}
 	
-	protected void onStop(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+	@Override
+	protected void onStop() {
+		super.onStop();
 		Log.i("Tracer","On Stop Executed");
 	}
 	
-	protected void onDestroy(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
 		Log.i("Tracer","On Destroy Executed");
 	}
 	
